8faf1ad8e8eacfb407f3e715b3bdb272990e44ad
takecarenis <take.care.nis@gmail.com>
2019-12-16 23:43:14 +0100

    Added basic project, created identity

--- AdminAssistant.Domain/Common/IEntity.cs
+++ AdminAssistant.Domain/Common/IEntity.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace AdminAssist.Domain.Common
+namespace AdminAssistant.Domain
 {
     public interface IEntity
     {
--- AdminAssistant.Domain/NewsletterHistory/NewsletterHistory.cs
+++ AdminAssistant.Domain/NewsletterHistory/NewsletterHistory.cs
@@ -1,9 +1,8 @@
-﻿using AdminAssist.Domain.Common;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace AdminAssist.Domain
+namespace AdminAssistant.Domain
 {
     public class NewsletterHistory : IEntity
     {

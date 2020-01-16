aa29899d24899b06d5d902e9003c31284951a84e
takecarenis <take.care.nis@gmail.com>
2020-01-16 16:23:11 +0100

    GetFiltered

--- AdminAssistant.Blog/ViewComponents/LatestPostViewComponent.cs
+++ AdminAssistant.Blog/ViewComponents/LatestPostViewComponent.cs
@@ -1,4 +1,5 @@
-﻿using AdminAssistant.Blog.Services.Interfaces;
+﻿using AdminAssistant.Blog.Models.DomainModel;
+using AdminAssistant.Blog.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -18,7 +19,7 @@ namespace AdminAssistant.Blog.ViewComponents
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
-
+            List<PostViewModel> latestPosts = _postService.GetFiltered()
             return View();
         }
     }

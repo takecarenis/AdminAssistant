5d86779cf78988b12ff89912afe0104fceadf88a
takecarenis <take.care.nis@gmail.com>
2020-02-06 17:12:18 +0100

    Icons, About, Contact

--- AdminAssistant.Blog/Controllers/HomeController.cs
+++ AdminAssistant.Blog/Controllers/HomeController.cs
@@ -40,6 +40,16 @@ namespace AdminAssistant.Blog.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
